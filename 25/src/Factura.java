bd662b2d72c066b8acb318975b981769f818d3c0
Apple <bobisoftsp@gmail.com>
2017-03-09 19:57:18 +0100

    Subida Tarea 25

---
+++ 25/src/Factura.java
@@ -0,0 +1,22 @@
+
+public class Factura {
+
+    String NumeroFactura ;
+    String Nombre ;
+    String Apellidos ;
+    String Calle ;
+    String Ciudad ;
+    
+    String ConseptoTF0;
+    String ConseptoTF1;
+    String ConseptoTF2;
+    String ConseptoTF3;
+    
+    int PrecioTF0;
+    int PrecioTF1;
+    int PrecioTF2;
+    int PrecioTF3;
+    
+    int CP ;
+    
+}
